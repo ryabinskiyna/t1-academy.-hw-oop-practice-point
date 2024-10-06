c3f07b8fcc13ec64813ae2279d44f28945705bda
Nikita Ryabinskii <ryabinskiyna@gmail.com>
2024-10-06 14:35:52 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point2D = new Point(3, 4);
+        System.out.println(point2D);
+
+        Point3D point3D = new Point3D(3, 4, 5);
+        System.out.println(point3D);
+    }
+}
